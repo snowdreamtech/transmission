bd2cdfd99a342b34958b01ffcbae29002dae6e31
snowdream <sn0wdr1am@qq.com>
2024-06-17 22:57:25 +0800

    feat: transmission

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-DOCKER_HUB_PROJECT=snowdreamtech/alpine
+DOCKER_HUB_PROJECT=snowdreamtech/transmission
 
-GITHUB_PROJECT=ghcr.io/snowdreamtech/alpine
+GITHUB_PROJECT=ghcr.io/snowdreamtech/transmission
 
 docker buildx build --platform=linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64,linux/ppc64le,linux/riscv64,linux/s390x \
 -t ${DOCKER_HUB_PROJECT}:latest \
